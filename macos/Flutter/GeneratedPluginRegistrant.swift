d24fc9d2320d8837198d692771209e56d4780ce1
IA7308 <naziqnaz13@gmail.com>
2024-05-15 02:30:35 +0700

    Integrasi Firebase dan Penambahan CRUD pada Timeline

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,14 +4,13 @@
 
 import FlutterMacOS
 import Foundation
+
+import cloud_firestore
 import file_selector_macos
 import firebase_core
-import cloud_firestore
-
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
